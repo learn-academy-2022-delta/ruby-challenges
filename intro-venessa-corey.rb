52dd03ef3ac273dc045ef55ba963576fcf5a1cdb
venessa <venessa.palmer@gmail.com>
2022-07-07 16:43:48 -0700

    intro-venessa-corey.rb

---
+++ intro-venessa-corey.rb
@@ -0,0 +1,31 @@
+p 5 + 5
+p 5 - 3
+p 6 * 4
+p 10 / 2
+p 2.5 + 3.2
+p 5.4 - 3.6
+p 7.8 * 5.4
+p 9.3 / 3.1
+p 5 % 2
+# p 8 / 0
+#p 0/0
+p my_fav_number = 7
+p my_fav_number / 2
+p someones_fav = 13
+p someones_fav = 7
+p my_fav_number - someones_fav
+p my_fav_number = 26 * my_fav_number
+name = 'venessa'
+p "Hi, #{name}"
+p name.upcase
+p name.reverse
+p name.include?('e')
+p name.capitalize
+shows = ['friends', 'the office', 'big sky', 'anime', 'new girl']
+p shows.length
+p shows.first
+p shows[3]
+p shows.reverse!
+fav_shows = []
+p fav_shows << shows[0]
+p fav_shows << shows[1]
