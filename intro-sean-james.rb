ca9973de1894c575654e64639a49e367811ab5f2
k0smo <sawallace7@gmail.com>
2022-07-07 15:51:37 -0700

    hand off ruby to james

---
+++ intro-sean-james.rb
@@ -0,0 +1,48 @@
+# Add, subtract, multiply and divide some numbers and floats.
+2+2
+3*3
+9/3
+10.5/2
+12.6/6
+10.0/3
+
+
+# Find the remainder of dividing two numbers using the modulo operator (%).
+10%3
+100%9
+
+# Divide a number by 0.
+100/0
+27/0
+# Divide 0 by 0.
+0/0
+
+# Set a variable called my_favorite_number equal to your favorite number
+my_favorite_number = 22
+my_favorite_number / 2
+
+
+# Calculate your favorite number divided by 2
+my_favorite_number.div(2) 
+
+
+# Set another variable called someones_favorite equal to 13
+someones_favorite = 13
+
+
+
+# Change the value of someones_favorite to 7
+
+
+
+someones_favorite = someones_favorite = 7
+
+
+# Subtract my_favorite_number from someones_favorite
+my_favorite_number = 22
+someones_favorite = 13
+my_favorite_number.subtract = someones_favorite
+
+
+
+# Change the value of my favorite number to be 26 times its current value
