78250edcbdfc180eed09f505e7612a2dc3cbef7a
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-07-07 14:24:45 -0700

    Ruby Work

---
+++ intro-leo-rick.rb
@@ -0,0 +1,67 @@
+# Challenges
+# Arithmetic Challenges
+# Remember that floats are numbers with a decimal point whereas integers have no fractional part so in Ruby, 1 and 1.0 are different types of things with slightly different behaviors.
+
+# Add, subtract, multiply and divide some numbers and floats.
+# Find the remainder of dividing two numbers using the modulo operator (%).
+# Divide a number by 0.
+
+# 7/ 0
+# puts 7 / 0
+#divided by 0 (ZeroDivisionError)
+
+# Divide 0 by 0.
+
+# 0/0 
+# p 0/0
+#divided by 0 (ZeroDivisionError)
+
+# Variables Challenges
+# Set a variable called my_favorite_number equal to your favorite number
+
+my_favorite_number = 85
+
+# Calculate your favorite number divided by 2
+
+my_favorite_number / 2
+p 85 / 2
+
+# Set another variable called someones_favorite equal to 13
+
+someones_favorite = 13
+
+# Change the value of someones_favorite to 7
+
+someones_favorite = 7
+p someones_favorite = 7
+
+# Subtract my_favorite_number from someones_favorite
+
+my_favorite_number - someones_favorite
+p my_favorite_number - someones_favorite
+
+# Change the value of my favorite number to be 26 times its current value
+
+my_favorite_number * 26
+p my_favorite_number * 26
+
+# Strings Challenges
+# Create a variable and return it in a sentence string interpolation
+
+num1 = 1
+num2 = 4
+p "The number #{num1} is less than #{num1 + num2}"
+
+# Create a variable that contains a string and test some of the Ruby string methods
+# .upcase
+# .reverse
+# .include?
+# .capitalize
+# Arrays Challenges
+# Create an array that contains the name of at least five TV shows
+# Find the length of the array
+# Return the first item in the array
+# Return the fourth item in the array
+# Permanently reverse the order of the array
+# Create a new empty array for your top favorite TV shows
+# From the full TV show array, add your top two favorite shows to the empty array
