245b071e249ea80cfbf871931295f04c9d992f2c
pkamano <p.amano1225@protonmail.com>
2022-07-11 12:00:46 -0700

    game plan

---
+++ jumanji-matrix.rb
@@ -0,0 +1,26 @@
+# Jumanji Matrix
+# Default Message: TRY AGAIN.
+# Have user input name = @User
+#Description: It's the witching hour, no moon. The immense amounts of rain muffle any sort of sounds, friendly or nefarious. A distorted beeping noise jolts you from your nightmare of coding JavaScript and being stuck in an infinite loop. As the beeping gets louder, you realize it's coming from your computer which turned on by itself. As you groggily stumble to see what the message on the screen says, you realize that the computer is asking for help. The message says "Will you help me @User? A reward awaits the person that hits ENTER. If you want to stay stuck in your infinite loop, then hit ESC and enjoy your day, maybe."
+    # Any action key make italicized.
+#@User hits ESC: "Good Luck being Stuck in the Loop"
+#@USer hits ENTER: "@User wakes up in a dark room disoriented.Looking for a light source, do you ASK Alexa to turn on the light? Or do you CLAP in hopes of a clapper?"
+    # Hint: "If only you could ASK a virtual assistant."", "CLAP HINT: A round of APPLAUSE brightens up a room" 
+    # @User ASKS Alexa, CLAPS hands: "The room is illuminated."
+# You see a pathway at the door. One going LEFT, one going RIGHT. Which direction do you GO?
+    # @User goes right: "You looped back to the dark room"
+        # Need to chose illumination again
+    # @User goes left: "You see two doors with keypads and yo need to chose a number"
+
+# Door Code: 3 or 9
+    # @User 3: Door of Ruby
+    # @User 9: Door of riches
+
+# 9 Room choose between two containers
+    #  treasure and secrets to coding  
+
+# 3 Room
+    # Samara from The Ring. Scream and lose voice but she gives you the ancients texts to coding and webbed hands.
+
+# End result of 9 : Wake up Rich coding guru, found treasure along the way. 
+# End result of 3: Wake up Coding guru but cannot speak with webbed hands
