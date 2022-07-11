3dae0e41c40eb0ce408aa4f1afc4abf5cf8297a0
Nicole <nicole.rivera061@gmail.com>
2022-07-11 14:32:10 -0700

    Nicole Drove

---
+++ text-game.rb
@@ -0,0 +1,97 @@
+# Lost in the Terminal
+
+# 3 Adventurers find themselves in the black void.
+
+
+# A prompt appears "Do you click the Red button or the Blue button?"
+
+# Red Button :
+# - Adventurers servers crashed and now you returned to the beginning of the Black Hole.
+
+    # Blue Button: 
+    # - Prompt: "Magic Web or Dark Web?"
+
+        # Magic Web:
+        #  - Prompt: "Do you save an elderly woman or a newborn baby?"
+
+            # Elderly Woman:
+            # "The ederly puts you in her Will testiment and she ends up passing away of natural causes a month later after saving her. She left you an estate and also $100 Billion. "
+
+            # Newborn :
+            # "The baby grows up and finds you 21 years later and offers to take you to lunch. Little do you know holding a grudge for not saving her grandmother instead of her as a newborn, she poisons your lemonade and with your last moments leans in to tell you that you made the wrong choice."
+
+    # Dark Web:
+    #  - FBI storms into your house, kicks your dog, and tackles you to the ground! You are now arrested and thrown in Federal Prison.
+
+
+
+
+
+
+
+# buttons = {red_button: 'Adventurers servers crashed and now you returned to the beginning of the Black Hole.', blue_button: 'Magic Web or Dark Web?'}
+
+# red_button = 'Adventurers servers crashed and now you returned to the beginning of the Black Hole.'
+# blue_button = 'Magic Web or Dark Web?'
+
+# def lost_in_the_terminal
+# puts 'Welcome to the Terminal.'
+# puts 'Do you click the Red Button or the Blue Button?'
+#     if red_button == red_button.gets.chomp
+#         p [:red_button]
+#     elsif blue_button == blue_button.gets.chomp
+#         p [:blue_button]
+#     end
+# end
+# p lost_in_the_terminal
+
+
+puts 'Welcome to the Terminal.'
+puts 'Do you click the Red Button or the Blue Button?'
+red_button = gets.chomp
+p red_button
+
+
+
+
+
+
+
+
+# class Game
+#     def initialize
+#       Prompt 'Welcome to the Terminal'
+#     end
+  
+#     def start_game
+#       while true
+#         puts "You have taken #{@steps_taken} steps."
+#         @steps_taken += 1
+#       end
+#     end
+#   end
+  
+#   Game.new
+
+
+# puts 'Welcome to the Terminal'
+# puts 'Do you click the Red Button or Blue Button?'
+# button = gets.chomp
+
+
+
+
+
+# # blue_button = gets.chomp
+
+
+
+
+# def which_button
+#     if red_button
+#         "server crashed"
+#     elsif blue_button
+#         "Magic Web or Dark Web"
+#     end 
+# end
+
